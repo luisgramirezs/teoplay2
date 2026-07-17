2b7224340a0e4a4b12ac795a1efd8e3b6ac47c4a
Luis Ramirez <luisguillermo80@gmail.com>
2026-07-17 17:42:29 -0500

    color de fondo de iconos de dimensiones

--- src/components/dimensions/DimensionCard.tsx
+++ src/components/dimensions/DimensionCard.tsx
@@ -91,6 +91,7 @@ const DimensionCard: React.FC<DimensionCardProps> = ({ dimensionKey, data, onCli
       onClick={clickable ? onClick : undefined}
     >
       <div className="flex items-center gap-3">
+        
         <div className={`w-10 h-10 rounded-xl flex items-center justify-center flex-shrink-0 ${meta.iconBgClass} text-white`}>
           <Icon className="w-5 h-5" />
         </div>
